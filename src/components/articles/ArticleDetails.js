621a238f4a28f0a919289c42182d0d486b91bf80
Celeste Cassel de Camps <celestec@rogers.com>
2020-08-06 17:00:17 -0400

    adding article title and content to details page

--- src/components/articles/ArticleDetails.js
+++ src/components/articles/ArticleDetails.js
@@ -15,12 +15,12 @@ export default function ArticleDetails(props) {
             <div className="card z-depth-0">
                 <div className="card-content">
                 <button className = "right btn red lighten-2 z-depth-0" onClick = { handleClick }>Favourite</button>
-                    <span className="card-title">Article Title</span>
+                    <span className="card-title">{article.title}</span>
                     <div className="card-action grey-text">
-                        <div>Authors: {article.authors} </div>
-                        <div>Published:</div>
+                        <div>Authors: { article.authors.map((author) => author + " " )} </div>
+                        <div>Published: </div>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eos deleniti suscipit provident veniam aut recusandae hic amet aliquid quis, error ipsa numquam ipsam tempora laborum ut voluptatem molestiae quos.</p>
+                    <p>{article.content}</p>
                 </div>
             </div>
         </div>
