da1f54dfd6e502771e6689d7ec6e3134bcf9ab38
Celeste Cassel de Camps <celestec@rogers.com>
2020-08-11 16:16:25 -0400

    Quiz template

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,7 +5,7 @@ export default function SignedInLinks() {
     return(
         <ul className = "right">
             <li><NavLink to ='/library'>Library</NavLink></li>
-            <li><NavLink to ='/favourites'>Favourites</NavLink></li>
+            <li><NavLink to ='/my-library'>My Library</NavLink></li>
             <li><NavLink to = '/quiz'>Quiz</NavLink></li>
             <li><NavLink to ='/'>Log Out</NavLink></li>
             <li><NavLink to ='/' className = 'btn btn-floating red lighten-2'>DJ</NavLink></li>
--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,13 +1,131 @@
-import React, { Component } from 'react'
+import React, { value, handleChange } from 'react'
 
-export class Quiz extends Component {
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Form from '@material-ui/core/FormLabel';
+
+class Quiz extends React.Component {
+
+    state = {
+        question1: 'none',
+        question2: 'none',
+        question3: 'none',
+        question4: 'none',
+        question5: 'none',
+        question6: 'none',
+        question7: 'none',
+        question8: 'none',
+        question9: 'none'
+    };
+
+   handleChange = key => (event, value) => {
+        this.setState({
+          [key]: value
+        });
+    };
+
+
     render() {
+
+        const { question1, question2, question3 } = this.state;
+
         return (
-            <div>
-                
-            </div>
+            <Form>
+                <FormControl component="fieldset">
+                    <FormLabel>1. Little interest or pleasure in doing things</FormLabel>
+                    <RadioGroup row aria-label="question1" name="question1" value={question1} onChange={this.handleChange('question1')}>
+                    <FormControlLabel value="Not at all1" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl> 
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>2. Feeling down, depressed, or hopeless</FormLabel>
+                    <RadioGroup row aria-label="question2" name="question2" value={question2} onChange={this.handleChange('question2')}>
+                    <FormControlLabel value="Not at all2" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>3. Trouble falling or staying asleep, or sleeping too much</FormLabel>
+                    <RadioGroup row aria-label="question3" name="question3" value={value} onChange={this.handleChange('question3')}>
+                    <FormControlLabel value="Not at all3" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>4. Feeling tired or having little energy</FormLabel>
+                    <RadioGroup row aria-label="question4" name="question4" value={value} onChange={this.handleChange('question4')}>
+                    <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>5. Poor appetite or overeating</FormLabel>
+                    <RadioGroup row aria-label="question5" name="question5" value={value} onChange={this.handleChange('question5')}>
+                    <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>6. Feeling bad about yourself – or that you are a failure or have let yourself or your family down</FormLabel>
+                    <RadioGroup row aria-label="question6" name="question6" value={value} onChange={this.handleChange('question6')}>
+                    <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>7. Trouble concentrating on things, such as reading the newspaper or watching television</FormLabel>
+                    <RadioGroup row aria-label="question7" name="question7" value={value} onChange={this.handleChange('question7')}>
+                    <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>8. Moving or speaking so slowly that other people could have noticed? Or the opposite – being so fidgety or restless that you have been moving around a lot more than usual</FormLabel>
+                    <RadioGroup row aria-label="question8" name="question8" value={value} onChange={this.handleChange('question8')}>
+                    <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+                <br></br>
+                <FormControl component="fieldset">
+                    <FormLabel>9. Thoughts that you would be better off dead or of hurting yourself insome way</FormLabel>
+                    <RadioGroup row aria-label="question9" name="question9" value={value} onChange={this.handleChange('question9')}>
+                    <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
+                    <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
+                    <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
+                    <FormControlLabel value="Nearly every day" control={<Radio />} label="Nearly every day" labelPlacement="top"/>
+                    </RadioGroup>
+                </FormControl>
+            </Form>
         )
     }
-}
+  } 
 
-export default Quiz
+  export default Quiz;
