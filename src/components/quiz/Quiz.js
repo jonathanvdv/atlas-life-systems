dd53db3f55eb2a3b22bd443dacf7ae1260c6665a
Celeste Cassel de Camps <celestec@rogers.com>
2020-08-11 16:34:10 -0400

    Update Quiz.js

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -25,12 +25,14 @@ class Quiz extends React.Component {
         this.setState({
           [key]: value
         });
+        event.preventDefault()
     };
 
 
     render() {
+        console.log(this.state)
 
-        const { question1, question2, question3 } = this.state;
+        const { question1, question2, question3, question4, question5, question6, question7, question8, question9 } = this.state;
 
         return (
             <Form>
@@ -56,7 +58,7 @@ class Quiz extends React.Component {
                 <br></br>
                 <FormControl component="fieldset">
                     <FormLabel>3. Trouble falling or staying asleep, or sleeping too much</FormLabel>
-                    <RadioGroup row aria-label="question3" name="question3" value={value} onChange={this.handleChange('question3')}>
+                    <RadioGroup row aria-label="question3" name="question3" value={question3} onChange={this.handleChange('question3')}>
                     <FormControlLabel value="Not at all3" control={<Radio />} label="Not at all" labelPlacement="top"/>
                     <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
                     <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
@@ -66,7 +68,7 @@ class Quiz extends React.Component {
                 <br></br>
                 <FormControl component="fieldset">
                     <FormLabel>4. Feeling tired or having little energy</FormLabel>
-                    <RadioGroup row aria-label="question4" name="question4" value={value} onChange={this.handleChange('question4')}>
+                    <RadioGroup row aria-label="question4" name="question4" value={question4} onChange={this.handleChange('question4')}>
                     <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
                     <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
                     <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
@@ -76,7 +78,7 @@ class Quiz extends React.Component {
                 <br></br>
                 <FormControl component="fieldset">
                     <FormLabel>5. Poor appetite or overeating</FormLabel>
-                    <RadioGroup row aria-label="question5" name="question5" value={value} onChange={this.handleChange('question5')}>
+                    <RadioGroup row aria-label="question5" name="question5" value={question5} onChange={this.handleChange('question5')}>
                     <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
                     <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
                     <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
@@ -86,7 +88,7 @@ class Quiz extends React.Component {
                 <br></br>
                 <FormControl component="fieldset">
                     <FormLabel>6. Feeling bad about yourself – or that you are a failure or have let yourself or your family down</FormLabel>
-                    <RadioGroup row aria-label="question6" name="question6" value={value} onChange={this.handleChange('question6')}>
+                    <RadioGroup row aria-label="question6" name="question6" value={question6} onChange={this.handleChange('question6')}>
                     <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
                     <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
                     <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
@@ -96,7 +98,7 @@ class Quiz extends React.Component {
                 <br></br>
                 <FormControl component="fieldset">
                     <FormLabel>7. Trouble concentrating on things, such as reading the newspaper or watching television</FormLabel>
-                    <RadioGroup row aria-label="question7" name="question7" value={value} onChange={this.handleChange('question7')}>
+                    <RadioGroup row aria-label="question7" name="question7" value={question7} onChange={this.handleChange('question7')}>
                     <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
                     <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
                     <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
@@ -106,7 +108,7 @@ class Quiz extends React.Component {
                 <br></br>
                 <FormControl component="fieldset">
                     <FormLabel>8. Moving or speaking so slowly that other people could have noticed? Or the opposite – being so fidgety or restless that you have been moving around a lot more than usual</FormLabel>
-                    <RadioGroup row aria-label="question8" name="question8" value={value} onChange={this.handleChange('question8')}>
+                    <RadioGroup row aria-label="question8" name="question8" value={question8} onChange={this.handleChange('question8')}>
                     <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
                     <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
                     <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
@@ -116,7 +118,7 @@ class Quiz extends React.Component {
                 <br></br>
                 <FormControl component="fieldset">
                     <FormLabel>9. Thoughts that you would be better off dead or of hurting yourself insome way</FormLabel>
-                    <RadioGroup row aria-label="question9" name="question9" value={value} onChange={this.handleChange('question9')}>
+                    <RadioGroup row aria-label="question9" name="question9" value={question9} onChange={this.handleChange('question9')}>
                     <FormControlLabel value="Not at all" control={<Radio />} label="Not at all" labelPlacement="top"/>
                     <FormControlLabel value="Several days" control={<Radio />} label="Several days" labelPlacement="top"/>
                     <FormControlLabel value="More than half the days" control={<Radio />} label="More than half the days" labelPlacement="top"/>
