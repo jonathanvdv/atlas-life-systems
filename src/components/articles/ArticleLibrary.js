d57c84c2abf5f99677992eb14ec21d52ef434e45
Celeste Cassel de Camps <celestec@rogers.com>
2020-08-13 13:55:03 -0400

    Update ArticleLibrary.js

--- src/components/articles/ArticleLibrary.js
+++ src/components/articles/ArticleLibrary.js
@@ -13,7 +13,7 @@ export default function ArticleLibrary({articles}) {
                         to={{
                             pathname: "/article/" + article.id,
                             state: { article: article }
-                        }}
+                        }} >
                         <ArticleSummary article = { article } key = { article.id }/>
                     </Link>
                 )
