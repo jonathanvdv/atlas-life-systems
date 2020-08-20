cb69063184559f5eed663196c38fc70d0e3f4d9d
Celeste Cassel de Camps <celestec@rogers.com>
2020-08-20 14:45:48 -0400

    added logged in user ID and date to quiz data

--- src/store/actions/quizActions.js
+++ src/store/actions/quizActions.js
@@ -2,10 +2,13 @@ export const addQuiz = (quiz) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // maker async call to database
         const firestore = getFirestore();
+        const firebase = getFirebase();
+        console.log(firebase.auth().currentUser.uid);
 
         firestore.collection('quizzes').add({
             ...quiz,
-            userId: 42069
+            userId: firebase.auth().currentUser.uid,
+            date: new Date()
             
         }).then(() => {
             dispatch({ type: 'ADD_QUIZ', quiz });
