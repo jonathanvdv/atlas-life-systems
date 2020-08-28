f7e8b5122009847a10fe5ae91dbbd7b92ff0e7d6
Dj <d3ejayiii@gmail.com>
2020-08-28 12:42:00 -0400

    Ranked articles
    it now works. dear allah it was so much freakin work to get. honestly, im glad its over.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 // This component is currently the homepage of the application
-import React, { Component } from 'react'
-import ArticleLibrary from '../articles/ArticleLibrary'
-import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
-import { firestoreConnect } from 'react-redux-firebase'
-import { compose } from 'redux'
+import React, { Component } from 'react';
+import ArticleLibrary from '../articles/ArticleLibrary';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import { dot } from 'mathjs';
 
 class Dashboard extends Component { 
+
     render () { 
         const { auth } = this.props;
         if (!auth.uid) return <Redirect to = '/signin' />;
@@ -14,57 +16,51 @@ class Dashboard extends Component {
         const filteredLibrarySize = 10;
         const { quizBitmaps } = this.props;
         const { articles } = this.props;
-        console.log("quizbitmaps",quizBitmaps);
-        console.log("articles",articles);
         
         // these arrays appear to be objects with properties as opposed to arrays
         // cant use array methods, need to use object methods
         var mostRecentQuiz = [];
+        var orderedMostRecentQuiz = [];
         var mostRecentQuizBitmap = [];
         if (quizBitmaps !== undefined && quizBitmaps.length > 0) {
             mostRecentQuiz = quizBitmaps[quizBitmaps.length - 1];
             delete mostRecentQuiz.date;
-            mostRecentQuizBitmap = mostRecentQuizBitmap.map(bit => {
-                return Number(bit)
-            })
-            console.log("mostrecentquiz",mostRecentQuiz);
-            console.log("mostrecentquizbitmap",mostRecentQuizBitmap);
+            Object.keys(mostRecentQuiz).sort().forEach((key) => {
+                orderedMostRecentQuiz[key] = mostRecentQuiz[key];
+              });
+            mostRecentQuizBitmap = Object.values(orderedMostRecentQuiz);
         } else {
             mostRecentQuiz = [];
             mostRecentQuizBitmap = [];
         }
-        console.log("mostrecentquiz",mostRecentQuiz);
-        console.log("mostrecentquizbitmap", mostRecentQuizBitmap);
 
         var articleRank;
-
         var rankedArticles = [];
         var sortedArticles = [];
-        if (articles !== undefined && articles.length > 0) {
-                articles.map(article => {
+        if (articles !== undefined && articles.length > 0 && mostRecentQuizBitmap.length > 0) {
+                rankedArticles = articles.map(article => {
                     if (mostRecentQuizBitmap.length === article.bitmap.length) {
-                        articleRank = mostRecentQuizBitmap.map(quizBitmap => {
-                            return quizBitmap * article.bitmap
-                        })
+                        articleRank = dot(mostRecentQuizBitmap, article.bitmap);
+                        let rankedArticle = Object.assign({}, {article: article}, {articleRank: articleRank});
                         return {
-                            ...article,
-                            articleRank: articleRank
+                            rankedArticle
                         }
                     } else {
-                        return article
+                        return article;
                     }
             })
-            sortedArticles = rankedArticles
-            .sort((a, b) => a.articleRank - b.articleRank)
-            .slice(0, filteredLibrarySize - 1);
-        } else {
-                rankedArticles = [];
-                sortedArticles = [];
+            
+            if (rankedArticles.length > 0){
+                sortedArticles = rankedArticles
+                .sort((b, a) => a.rankedArticle.articleRank - b.rankedArticle.articleRank)
+                .slice(0, filteredLibrarySize - 1);
+            }
         }
-        console.log("rankedarticles",rankedArticles)
-
-        console.log("sortedarticles",sortedArticles)
-
+        if (sortedArticles.length > 0) {
+            sortedArticles = sortedArticles.map(article => {
+                return article.rankedArticle.article
+            })
+        }
         return (
             <div className="dashboard container">
                 <div className="row">
@@ -76,7 +72,6 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-    console.log(state); 
     return { 
         articles: state.firestore.ordered.articles,
         quizBitmaps: state.firebase.profile.quizBitmaps,
